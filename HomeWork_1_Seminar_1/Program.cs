d77b174409309f734fec64a055ac659665611505
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-16 14:42:38 +0500

    creat prog_1

---
+++ HomeWork_1_Seminar_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 89098;
+int b = 658546546;
+
+if (a>b)
+{
+    System.Console.WriteLine("max number = a, min number = b");
+}
+if (a<b)
+{
+    System.Console.WriteLine("max number = b, min number = a");
+}
