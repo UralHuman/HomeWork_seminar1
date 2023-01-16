2cd9554dd39495cc8619ec59de37e1a15fe8078f
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-16 15:44:54 +0500

    creat prog_3

---
+++ HomeWork_3_Seminar_1/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите число: ");
+
+int a = System.Convert.ToInt32(Console.ReadLine());
+
+
+if (a % 2 == 0 )
+{
+    System.Console.WriteLine("even number");
+
+}
+else 
+{
+    System.Console.WriteLine("not even number");
+}
+
