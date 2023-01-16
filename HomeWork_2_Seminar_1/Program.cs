8ca93964d6c5c41696c9e91e0120fce885af4af5
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-16 14:28:41 +0500

    creat programm_2

---
+++ HomeWork_2_Seminar_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 46;
+int b = 4734;
+int c = 2;
+
+if (a>b && a>c)
+{
+    System.Console.WriteLine("max nember = a");
+}
+
+if (b>a && b>c)
+{
+    System.Console.WriteLine("max number = b");
+}
+
+if (c>a && c>b)
+{
+    System.Console.WriteLine("max number = c");
+}
